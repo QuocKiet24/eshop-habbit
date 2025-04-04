1b826bf68bc4d2e33f72ad66deb694e9497ef522
trquockiet24 <trquockiet24@gmail.com>
2025-04-04 13:02:10 +0700

    done admin UI

--- frontend/src/components/Admin/AdminSideBar.jsx
+++ frontend/src/components/Admin/AdminSideBar.jsx
@@ -57,7 +57,7 @@ const AdminSideBar = () => {
           <span>Orders</span>
         </NavLink>
         <NavLink
-          to="/admin/shop"
+          to="/"
           className={({ isActive }) =>
             isActive
               ? "bg-gray-700 text-white py-3 px-4 rounded flex items-center space-x-2"
